b5afcfb0fabd9a24a5b27825721d4f2d342dca25
Alessio Scarfone <alessioscarfone93@gmail.com>
2018-06-26 14:02:03 +0200

    list reference-count

--- src/mr/CountReference.java
+++ src/mr/CountReference.java
@@ -2,6 +2,8 @@ package mr;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
@@ -9,8 +11,11 @@ import javax.xml.stream.XMLStreamReader;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -19,8 +24,11 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class CountReference {
+	private static IntWritable one = new IntWritable(1);
 
-	public static class Mapper1 extends Mapper<LongWritable, Text, Text, Text> {
+	public static class Mapper1 extends Mapper<LongWritable, Text, PageKey, IntWritable> {
+		private static String regex = "\\[\\[(.*?)\\]\\]";
+		private static Pattern pattern = Pattern.compile(regex);
 
 		@Override
 		protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
@@ -49,24 +57,53 @@ public class CountReference {
 					}
 				}
 				reader.close();
-				context.write(new Text(recordKey.trim()), new Text(recordValue.trim()));
-
+				if (!recordKey.equals("") && !recordValue.equals(""))
+					parseText(recordKey, recordValue, context);
+			
 			} catch (Exception e) {
 				throw new IOException(e);
 			}
 		}
-	}
 
-	public static class Reducer1 extends Reducer<Text, Text, Text, Text> {
-
-		@Override
-		protected void reduce(Text key, Iterable<Text> values, Context context)
-				throws IOException, InterruptedException {
-			for (Text t : values) {
-				context.write(new Text("KEY:" + key.toString()), new Text("TEXT:" + t));
+		private void parseText(String title, String text, Context context) throws IOException, InterruptedException {
+			Matcher matcher = pattern.matcher(text);
+			while (matcher.find()) {
+				String reference = matcher.group(1);
+				if (reference.contains("|")) {
+					reference = reference.split("\\|")[0];
+				}
+				if(reference.contains("File:") || reference.contains("Categoria:") || reference.contains("Aiuto:") || reference.contains("s:"))
+					continue;
+				reference = reference.replaceAll("\\(|\\)|,|;|:", "");
+				PageKey pk = new PageKey(reference.trim(), title.toString().trim());
+				context.write(pk, one);
 			}
 		}
+	}
 
+	public static class Reducer1 extends Reducer<PageKey, IntWritable, Text, IntWritable> {
+
+		@Override
+		protected void reduce(PageKey key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			context.write(new Text(key.pageTo), one);
+		}
+	}
+	
+	
+	
+	public static class PageKeyGroupComparator extends WritableComparator {
+
+		protected PageKeyGroupComparator() {
+			super(PageKey.class, true);
+		}
+
+		@Override
+		public int compare(WritableComparable a, WritableComparable b) {
+			PageKey ac = (PageKey) a;
+			PageKey bc = (PageKey) b;
+			return ac.compareTo(bc);
+		}
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -74,9 +111,11 @@ public class CountReference {
 		conf.set("xmlinput.start", "<page>");
 		conf.set("xmlinput.end", "</page>");
 		Job job = Job.getInstance(conf, "Count Reference");
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+		
+		job.setOutputKeyClass(PageKey.class);
+		job.setOutputValueClass(IntWritable.class);
 		job.setMapperClass(Mapper1.class);
+//		job.setGroupingComparatorClass(PageKeyGroupComparator.class);
 		job.setReducerClass(Reducer1.class);
 
 		job.setInputFormatClass(XmlInputFormat.class);
---
+++ src/mr/PageKey.java
@@ -0,0 +1,50 @@
+package mr;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableUtils;
+
+public class PageKey implements WritableComparable<PageKey> {
+	String pageTo;
+	String pageFrom;
+
+	public PageKey() {
+	}
+
+	public PageKey(String pageTo, String pageFrom) {
+		super();
+		this.pageTo = pageTo;
+		this.pageFrom = pageFrom;
+	}
+
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		pageTo = WritableUtils.readString(in);
+		pageFrom = WritableUtils.readString(in);
+	}
+
+	@Override
+	public void write(DataOutput out) throws IOException {
+		WritableUtils.writeString(out, pageTo);
+		WritableUtils.writeString(out, pageFrom);
+	}
+
+	@Override
+	public int compareTo(PageKey o) {
+		int cmp = pageTo.compareTo(o.pageTo);
+		if(cmp == 0) 
+			cmp = pageFrom.compareTo(pageFrom);
+		return cmp;
+	}
+
+	@Override
+	public String toString() {
+		return  pageTo+"|"+pageFrom;
+	}
+	
+	
+
+}
